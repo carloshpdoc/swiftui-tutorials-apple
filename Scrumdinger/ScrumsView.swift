0873883278288a3777ff53903a8dbc75c88e8e9c
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2021-01-11 18:04:36 -0300

    add navigation

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,19 +9,26 @@ import SwiftUI
 
 struct ScrumsView: View {
     let scrums: [DailyScrum]
-
+    
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.color)
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }.listRowBackground(scrum.color)
             }
         }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Image(systemName: "plus")
+        })
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.data)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
     }
 }
